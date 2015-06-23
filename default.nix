e789579c467dc9aab053abd48fa37cc853ba3122
James Thompson <james@orphid.com>
2015-06-23 16:04:24 -0700

    update default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
 { mkDerivation, base, bytestring, cereal, containers, digest
 , either, hspec, lens, mtl, network, QuickCheck, random
-, resource-pool, stdenv, transformers
+, resource-pool, stdenv, time, transformers
 }:
 mkDerivation {
   pname = "milena";
-  version = "0.2.0.0";
+  version = "0.2.1.0";
   src = ./.;
   buildDepends = [
     base bytestring cereal containers digest either lens mtl network
-    random resource-pool transformers
+    random resource-pool time transformers
   ];
   testDepends = [ base bytestring hspec network QuickCheck ];
   description = "A Kafka client for Haskell";
